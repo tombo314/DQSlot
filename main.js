357c5ccf21e6aeec5b32776c282020a21c45e903
tombo314 <u_tombo@icloud.com>
2024-02-06 01:06:44 +0900

    Add hit, skillList, Remove stop

--- main.js
+++ main.js
@@ -1,38 +1,52 @@
 let elemSlot = document.getElementById("js-slot");
 let elemTextSlot = document.getElementsByClassName("js-text-slot");
 let elemButtonStart = document.getElementById("js-button-start");
-let elemButtonStop = document.getElementById("js-button-stop");
 
 elemButtonStart.onclick = ()=>{
     elemButtonStart.disabled = true;
-    elemButtonStop.disabled = false;
     for (let text of elemTextSlot){
         text.classList.add("slot-mode");
         let duration = 1;
         text.style.animationDuration = `${duration}s`;
         text.style.animationIterationCount = "infinite";
     }
-};
 
-elemButtonStop.onclick = ()=>{
-    elemButtonStop.disabled = true;
     setTimeout(() => {
         let unit = -24;
-        let rand = unit*Math.trunc(Math.random()*15);
+        let rand = unit*(10+Math.trunc(Math.random()*10));
         for (let text of elemTextSlot){
             text.classList.remove("slot-mode");
             text.style.transform = `translateY(${rand}px)`;
         }
         elemButtonStart.disabled = false;
-    }, 2000);
+    }, 4000);
 };
 
 let registerSkill = (skillList)=>{
     let skillNum = 10;
     for (let i=0; i<skillNum*3; i++){
+        if (i==2){
+            let elem = document.createElement("div");
+            elem.setAttribute("class", "hit");
+            elemSlot.appendChild(elem);
+        }
         let elem = document.createElement("p");
         elem.setAttribute("class", "js-text-slot");
-        elem.textContent = skillList[i%5];
+        elem.textContent = skillList[i%skillNum];
         elemSlot.appendChild(elem);
     }
 };
+
+let skillList = [
+    "メラ",
+    "ヒャド",
+    "ギラ",
+    "バギ",
+    "イオ",
+    "ドルマ",
+    "デイン",
+    "ザキ",
+    "ルカニ",
+    "ホイミ"
+];
+registerSkill(skillList);
