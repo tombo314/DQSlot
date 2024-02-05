77ab235956b5465f1b7ca19b23160901abc6485b
tombo314 <u_tombo@icloud.com>
2024-02-06 00:50:40 +0900

    Add registerSkill, stop, start

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
+let elemSlot = document.getElementById("js-slot");
 let elemTextSlot = document.getElementsByClassName("js-text-slot");
 let elemButtonStart = document.getElementById("js-button-start");
 let elemButtonStop = document.getElementById("js-button-stop");
 
 elemButtonStart.onclick = ()=>{
+    elemButtonStart.disabled = true;
+    elemButtonStop.disabled = false;
     for (let text of elemTextSlot){
-        text.classList.add("slot-move");
+        text.classList.add("slot-mode");
+        let duration = 1;
+        text.style.animationDuration = `${duration}s`;
+        text.style.animationIterationCount = "infinite";
     }
 };
 
 elemButtonStop.onclick = ()=>{
-    for (let text of elemTextSlot){
-        text.classList.remove("slot-move");
+    elemButtonStop.disabled = true;
+    setTimeout(() => {
+        let unit = -24;
+        let rand = unit*Math.trunc(Math.random()*15);
+        for (let text of elemTextSlot){
+            text.classList.remove("slot-mode");
+            text.style.transform = `translateY(${rand}px)`;
+        }
+        elemButtonStart.disabled = false;
+    }, 2000);
+};
+
+let registerSkill = (skillList)=>{
+    let skillNum = 10;
+    for (let i=0; i<skillNum*3; i++){
+        let elem = document.createElement("p");
+        elem.setAttribute("class", "js-text-slot");
+        elem.textContent = skillList[i%5];
+        elemSlot.appendChild(elem);
     }
 };
