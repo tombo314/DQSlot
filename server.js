983ad3108b58b92c190fa7af5492a1bb40538281
tombo314 <u_tombo@icloud.com>
2024-02-06 16:23:44 +0900

    Update index -> battle, Add battle

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ let fs = require("fs");
 let server = http.createServer((req, res)=>{
     if (req.url=="/"){
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(fs.readFileSync("index.html"));
-    } else if (req.url=="/stylesheet/main.css"){
+        res.end(fs.readFileSync("top/top.html"));
+    } else if (req.url=="/top/stylesheet/top.css"){
         res.writeHead(200, {"Content-Type": "text/css"});
-        res.end(fs.readFileSync("stylesheet/main.css"));
-    } else if (req.url=="/main.js"){
+        res.end(fs.readFileSync("top/stylesheet/top.css"));
+    } else if (req.url=="/top/top.js"){
         res.writeHead(200, {"Content-Type": "text/javascript"});
-        res.end(fs.readFileSync("main.js"));
-    } else if (req.url=="/favicon.ico"){
+        res.end(fs.readFileSync("top/top.js"));
+    }
+    else if (req.url=="/battle"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(fs.readFileSync("battle/battle.html"));
+    } else if (req.url=="/battle/stylesheet/battle.css"){
+        res.writeHead(200, {"Content-Type": "text/css"});
+        res.end(fs.readFileSync("battle/stylesheet/battle.css"));
+    } else if (req.url=="/battle/battle.js"){
+        res.writeHead(200, {"Content-Type": "text/javascript"});
+        res.end(fs.readFileSync("battle/battle.js"));
+    }
+    else if (req.url=="/favicon.ico"){
         res.end();
     }
 });
