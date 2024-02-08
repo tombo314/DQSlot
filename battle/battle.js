49c297508806ed396cf449f7b82627cea6051b15
tombo314 <u_tombo@icloud.com>
2024-02-08 16:07:57 +0900

    Update クラス化、関数化

--- battle/battle.js
+++ battle/battle.js
@@ -1,56 +1,96 @@
-let elemSlot = document.getElementById("js-slot");
-let elemTextSlot = document.getElementsByClassName("js-text-slot");
-let elemButtonStart = document.getElementById("js-button-start");
-
-elemButtonStart.onclick = ()=>{
-    elemButtonStart.disabled = true;
-    for (let text of elemTextSlot){
-        text.classList.add("slot-mode");
-        let duration = 1;
-        text.style.animationDuration = `${duration}s`;
-        text.style.animationIterationCount = "infinite";
+class Slot {
+    constructor(choice){
+        this.choice = choice;
+        this.choiceNum = choice.length;
     }
 
-    setTimeout(() => {
-        let unit = -24;
-        let rand = unit*(10+Math.trunc(Math.random()*10));
-        for (let text of elemTextSlot){
-            text.classList.remove("slot-mode");
-            text.style.transform = `translateY(${rand}px)`;
-        }
-        elemButtonStart.disabled = false;
-    }, 4000);
-};
+    registerChoice(){
+        let elemSlot = document.getElementById("js-slot");
+        let choiceNum = this.choice.length;
 
-let registerSkill = (skillList)=>{
-    let skillNum = 10;
-    let len = skillList.length;
-    for (let _=0; _<skillNum-len; _++){
-        skillList.push("攻撃");
-    }
-    for (let i=0; i<skillNum*3; i++){
-        if (i==2){
-            let elem = document.createElement("div");
-            elem.setAttribute("class", "hit");
+        for (let i=0; i<choiceNum*5; i++){
+            if (i==2){
+                let elem = document.createElement("div");
+                elem.setAttribute("class", "hit");
+                elemSlot.appendChild(elem);
+            }
+            let elem = document.createElement("p");
+            elem.setAttribute("class", "js-text-slot");
+            elem.textContent = this.choice[i%choiceNum];
             elemSlot.appendChild(elem);
         }
-        let elem = document.createElement("p");
-        elem.setAttribute("class", "js-text-slot");
-        elem.textContent = skillList[i%skillNum];
-        elemSlot.appendChild(elem);
     }
-};
 
-let skillList = [
-    "メラ",
-    "ヒャド",
-    "ギラ",
-    "バギ",
-    "イオ",
-    "ドルマ",
-    "デイン",
-    "ザキ",
-    "ルカニ",
-    "ホイミ"
+    setKeyframes(){
+        let keyframes = `
+            @keyframes slot{
+                0%{
+                    transform: translateY(${-48*this.choiceNum}px);
+                }
+                100%{
+                    transform: translateY(0);
+                }
+            }
+        `;
+        let style = document.getElementsByTagName("style")[0];
+        style.insertAdjacentHTML("afterbegin", keyframes);
+    }
+}
+
+function rand(n){
+    return Math.trunc(Math.random()*n);
+}
+
+function enableButtonStart(choiceNum){
+    let elemButtonStart = document.getElementById("js-button-start");
+    let elemTextSlot = document.getElementsByClassName("js-text-slot");
+
+    elemButtonStart.onclick = ()=>{
+        elemButtonStart.disabled = true;
+        for (let text of elemTextSlot){
+            text.classList.add("slot-mode");
+            let loopDuration = choiceNum*0.14;
+            text.style.animationDuration = `${loopDuration}s`;
+            text.style.animationIterationCount = "infinite";
+        }
+        let spinDuration = 3000;
+        setTimeout(() => {
+            let unit = -24;
+            let rand = unit*(choiceNum+rand(choiceNum));
+            for (let text of elemTextSlot){
+                text.classList.remove("slot-mode");
+                text.style.transform = `translateY(${rand}px)`;
+            }
+            let restartDuration = 800;
+            setTimeout(()=>{
+                elemButtonStart.disabled = false;
+            }, restartDuration);
+        }, spinDuration);
+    };
+}
+
+let choice = [
+    "所持金+10%",
+    "所持金増加量+10%",
+    "所持金+30%",
+    "スロット間隔短縮30秒",
+    "所持金+50%",
+    "ショップ1回",
+    "所持金増加量+30%",
+    "スロット強化30秒",
 ];
-registerSkill(skillList);
+
+enableButtonStart(choice.length);
+
+let slot = new Slot(choice);
+slot.registerChoice();
+slot.setKeyframes();
+
+let elemMoney = document.getElementById("js-money");
+
+let money = 0;
+let moneyIncrease = 1;
+// setInterval(() => {
+//     money += moneyIncrease;
+//     elemMoney.textContent = money;
+// }, 1000);
